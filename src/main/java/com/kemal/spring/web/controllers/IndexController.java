17be149cdb5afa570db44196177707658e9031a4
Kemal Žigović <kvant800@gmail.com>
2017-11-23 19:29:28 +0100

    Adding missing files to git
    Signed-off-by: Kemal Žigović <kvant800@gmail.com>

---
+++ src/main/java/com/kemal/spring/web/controllers/IndexController.java
@@ -0,0 +1,34 @@
+package com.kemal.spring.web.controllers;
+
+import com.kemal.spring.web.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * Created by Keno&Kemo on 30.09.2017..
+ */
+
+@Controller
+@RequestMapping("")
+public class IndexController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index (){
+        return "website/index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login (){
+        return "website/login";
+    }
+
+    @GetMapping(value = "/register")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("userDto", userDto);
+        return "website/register";
+    }
+}
