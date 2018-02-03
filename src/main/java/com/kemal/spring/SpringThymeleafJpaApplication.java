de211e559a8d80ca056e858d2d1e212ba5a5c73b
Kemal Žigović <kvant800@gmail.com>
2018-02-03 21:56:09 +0100

    Refactoring
    Signed-off-by: Kemal Žigović <kvant800@gmail.com>

--- src/main/java/com/kemal/spring/SpringThymeleafJpaApplication.java
+++ src/main/java/com/kemal/spring/SpringThymeleafJpaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringThymleafJpaApplication {
+public class SpringThymeleafJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringThymleafJpaApplication.class, args);
+		SpringApplication.run(SpringThymeleafJpaApplication.class, args);
 	}
 
 
