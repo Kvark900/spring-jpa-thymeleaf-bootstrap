e43db52bb8dc1339b4359b6b0ce8ec6f9c9b8406
Kemal Žigović <kvant800@gmail.com>
2018-03-02 12:34:52 +0100

    Making active menu items
    Signed-off-by: Kemal Žigović <kvant800@gmail.com>

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,3 +5,7 @@ var menuItem = qs.replace("http://localhost:8080","");
 //making active links
 $("a[href='" + menuItem + "']").parent("li").addClass("active");
 console.log(menuItem);
+
+if(menuItem.includes("adminPage")){
+    $("a[href='/adminPage']").parent("li").addClass("active");
+}
