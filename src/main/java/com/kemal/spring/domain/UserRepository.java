3b12fbb9c5eb8b28c0abfba7f292c8a1d505d564
Kemal Žigović <kvant800@gmail.com>
2018-06-20 20:51:44 +0200

    Eagerly fetching users
    Signed-off-by: Kemal Žigović <kvant800@gmail.com>

--- src/main/java/com/kemal/spring/domain/UserRepository.java
+++ src/main/java/com/kemal/spring/domain/UserRepository.java
@@ -1,7 +1,11 @@
 package com.kemal.spring.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Keno&Kemo on 30.09.2017..
@@ -10,7 +14,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = (:email)")
+    User findByEmail(@Param("email") String email);
+
+
+
+
     User findById(Long id);
     User findByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles")
+    List<User> findAllEagerly();
 }
